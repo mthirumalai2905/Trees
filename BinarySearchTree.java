e6aa628ecf24b92c7b0b9ae3b7286480138427ff
Thirumalainambi <thiru@thiru.com>
2024-04-01 14:52:33 +0530

    added binarysearchtree

---
+++ BinarySearchTree.java
@@ -0,0 +1,52 @@
+public class BinarySearchTree {
+    TreeNode root;
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public void insert(int val) {
+        root = insertRecursive(root, val);
+    }
+
+    public TreeNode insertRecursive(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
+
+        if (val < root.val) {
+            root.left = insertRecursive(root.left, val);
+        } else if (val > root.val) {
+            root.right = insertRecursive(root.right, val);
+        }
+
+        return root;
+    }
+
+    public void inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrderTraversal(root.left);
+        System.out.print(root.val + " "); // Output the value without "->"
+        inOrderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(8);
+        bst.insert(3);
+        bst.insert(10);
+        bst.insert(1);
+        bst.insert(6);
+        bst.insert(14);
+        bst.insert(4);
+        bst.insert(7);
+        bst.insert(13);
+
+        System.out.println("Inorder Traversal:");
+        bst.inOrderTraversal(bst.root);
+    }
+}
